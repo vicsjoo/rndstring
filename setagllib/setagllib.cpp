a362d91588bad85f7e4c792d030de713984a0811
João Victor Pereira da Silva <vics@ynko.onmicrosoft.com>
2022-04-03 23:03:58 -0300

    Add project files.

---
+++ setagllib/setagllib.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+typedef unsigned char BYTE;
+static BYTE satan[9] = { 0x73, 0x65, 0x74, 0x61, 0x67, 0x6c, 0x6c, 0x69, 0x62 };
+void satanic();
+int main()
+{
+    for (size_t i = 0; i < 6; i++)
+    {
+        satanic();
+    }
+    
+    
+    
+}
+void satanic() {
+   
+    char str[sizeof(satan) + 1];
+    memcpy(str, satan, sizeof(satan));
+    str[sizeof(satan)] = '\0';
+    std::cout << str+ std::string("\n");       
+}
