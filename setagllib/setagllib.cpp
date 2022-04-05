33d2e1e3e5bec749a9d0b9474afb0f27a7c843e7
João Victor Pereira da Silva <vics@ynko.onmicrosoft.com>
2022-04-05 01:44:46 -0300

    removed unecessary comments

--- setagllib/setagllib.cpp
+++ setagllib/setagllib.cpp
@@ -7,90 +7,87 @@
 #include <ctime>
 
 typedef unsigned char BYTE;
-
-
 static BYTE satan[9] = { 0x73, 0x65, 0x74, 0x61, 0x67, 0x6c, 0x6c, 0x69, 0x62 };
 void satanic();
 void gibberish();
 void fakehash();
 std::string RandomNumeric(int len);
 std::string RandomString(int len);
+
 int main()
 {
 
-    srand(static_cast<unsigned int>(time(NULL))<<10);
+	srand(static_cast<unsigned int>(time(NULL)) << 10);
 
 
-    /** backup cout buffer and redirect to out.txt **/
-    std::ofstream out("out.txt");
+	/** backup cout buffer and redirect to out.txt **/
+	std::ofstream out("out.txt");
 
-    auto* coutbuf = std::cout.rdbuf();
-    std::cout.rdbuf(out.rdbuf());
+	auto* coutbuf = std::cout.rdbuf();
+	std::cout.rdbuf(out.rdbuf());
 
-    for (size_t i = 0; i < (10000); i++)
-    {
-     //   satanic();
-     //   gibberish();
-        fakehash();
+	for (size_t i = 0; i < (10000); i++)
+	{
+		//   satanic();
+		//   gibberish();
+		fakehash();
 
-    }
-    // system("pause");
-    
-    
+	}
+	// system("pause");
+
+
 }
 void satanic() {
-    char str[sizeof(satan) + 1];
-    memcpy(str, satan, sizeof(satan));
-    str[sizeof(satan)] = '\0';
+	char str[sizeof(satan) + 1];
+	memcpy(str, satan, sizeof(satan));
+	str[sizeof(satan)] = '\0';
 
-    if ((rand() % 2) + 1 == 2) {
-        std::reverse(str, str + strlen(str));
-    }
-    std::cout << str << std::endl;
+	if ((rand() % 2) + 1 == 2) {
+		std::reverse(str, str + strlen(str));
+	}
+	std::cout << str << std::endl;
+	// std::cout << str+ std::string("\n");   
 }
-    // std::cout << str+ std::string("\n");     
+
 
 
 void gibberish() {
 
-  //  char hell[20] = "";
 
-    for (size_t i = 0; i < 1; i++)
-    {
- //       hell[i] << (char)RandomString(!);
-          std::cout << RandomString(10) << std::endl;
-          std::cout << RandomNumeric(10) << std::endl;
-    //    satanic();
-    }
+	for (size_t i = 0; i < 1; i++)
+	{
+		std::cout << RandomString(10) << std::endl;
+		std::cout << RandomNumeric(10) << std::endl;
 
- //   std::cout << hell;
-   
+	}
+
+
 }
 void fakehash() {
-    std::cout << RandomString(1000) << std::endl;
+	std::cout << RandomString(1000) << std::endl;
 }
 
-std:: string RandomNumeric(int len)
+std::string RandomNumeric(int len)
 {
-    std::string str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    std::string newstr;
-    int pos;
-    while (newstr.size() != len -1) {
-        pos = ((rand() % (str.size() - 52)));
-        newstr += str.substr(pos, 1);
-    }
-    return newstr;
-    
+	std::string str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	std::string newstr;
+	int pos;
+	while (newstr.size() != len - 1) {
+		pos = ((rand() % (str.size() - 52)));
+		newstr += str.substr(pos, 1);
+	}
+	return newstr;
+
 }
 
 std::string RandomString(int len)
 {
-    std::string str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    std::string newstr;
-    int pos;
-    while (newstr.size() != len) {
-        pos = ((rand() % (str.size())));
-        newstr += str.substr(pos, 1);
-    }
-    return newstr;
+	std::string str = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	std::string newstr;
+	int pos;
+	while (newstr.size() != len) {
+		pos = ((rand() % (str.size())));
+		newstr += str.substr(pos, 1);
+	}
+	return newstr;
 }
